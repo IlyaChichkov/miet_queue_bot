6732e0478a7a5c4d2142c1892a1727301a76747a
AenclaveGames <55506834+IlyaChichkov@users.noreply.github.com>
2023-10-03 21:47:24 +0300

    feat: Добавил лог при присоединении к комнате и создании комнаты

--- handlers/welcome.py
+++ handlers/welcome.py
@@ -2,6 +2,8 @@ from aiogram import Router, F, types
 from aiogram.fsm.state import StatesGroup, State
 from aiogram.filters import Command
 from aiogram.fsm.context import FSMContext
+
+from bot_logging import log_user_info
 from firebase import db_create_room, user_join_room, enter_queue
 import re
 
@@ -37,8 +39,9 @@ async def create_room(message: types.Message, state: FSMContext):
     if create_room_input_reg(message.text):
         args = message.text.split('#')
         result = await db_create_room(message.from_user.id, args[0], args[1])
-        print(result)
-        if 'room' in result:
+        is_room_created = 'room' in result
+        log_user_info(message.from_user.id, f'Try create room, name: {args[1]}, password: {args[0]}, result: {is_room_created}')
+        if is_room_created:
             await message.answer(f"Вы создали комнату {args[1]}.")
             await state.set_state(RoomVisiterState.ROOM_WELCOME_SCREEN)
             await welcome_room_state(message)
@@ -54,32 +57,29 @@ async def create_room(message: types.Message, state: FSMContext):
 async def join_room(message: types.Message, state: FSMContext):
     filter_join_code = re.findall(r"^[0-9]+$", message.text)
 
-    print('2)', message.text)
-    print('1)', len(filter_join_code) > 0 and len(filter_join_code[0]) == 4)
-    print('2)', len(message.text) == 7)
+    check_user_code = len(filter_join_code) > 0 and len(filter_join_code[0]) == 4
+    check_mod_code = len(message.text) == 7
 
-    if len(filter_join_code) > 0 and len(filter_join_code[0]) == 4:
+    if check_user_code:
         joined_room = await user_join_room(message.from_user.id, message.text, 'user')
-        print(joined_room)
+        room_name = joined_room['room']['name']
         if 'room' in joined_room:
-            print('joined_room', joined_room)
-            print('room', joined_room['room'])
             if 'queue_on_join' in joined_room['room'] and joined_room['room']['queue_on_join'] == True:
                 await enter_queue(message.from_user.id)
 
+            log_user_info(message.from_user.id, f'Joined room, name: {room_name} as user')
             await state.set_state(RoomVisiterState.ROOM_WELCOME_SCREEN)
             await welcome_room_state(message)
-            # await message.answer(f"Студент {message.from_user.username}\nДобро пожаловать в комнату {joined_room['room']['name']}")
         else:
             await message.answer(f"Ошибка подключения к комнате. {joined_room['error']}")
             await state.set_state(WelcomeState.WELCOME_SCREEN)
-    elif len(message.text) == 7:
+    elif check_mod_code:
         joined_room = await user_join_room(message.from_user.id, message.text, 'moderator')
-        print(joined_room)
+        room_name = joined_room['room']['name']
         if 'room' in joined_room:
+            log_user_info(message.from_user.id, f'Joined room, name: {room_name} as moderator')
             await state.set_state(RoomVisiterState.ROOM_WELCOME_SCREEN)
             await welcome_room_state(message)
-            # await message.answer(f"Модератор {message.from_user.username}\nДобро пожаловать в комнату {joined_room['room']['name']}")
         else:
             await message.answer(f"Ошибка подключения к комнате. {joined_room['error']}")
             await state.set_state(WelcomeState.WELCOME_SCREEN)
