d7cb48585e87bb4c5d102b45a0ba9cd47b2e6372
AenclaveGames <55506834+IlyaChichkov@users.noreply.github.com>
2023-10-10 23:06:52 +0300

    repo: Убрал ненужный файл

--- handlers/update_data.py
+++
@@ -1,8 +0,0 @@
-from aiogram import Router, F, types
-from bot import bot
-
-router = Router()
-
-
-async def send_update_msg(user_id, message: types.Message):
-    await bot.send_message(user_id, message)
