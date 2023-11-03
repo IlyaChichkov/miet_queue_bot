53059fb08fbd9d685f02cb0ad13fd6f555a16a11
AenclaveGames <55506834+IlyaChichkov@users.noreply.github.com>
2023-11-03 17:05:13 +0300

    feat: Запись лога бота в отдельные файлы в папке

--- bot_logging.py
+++ bot_logging.py
@@ -1,20 +1,15 @@
 import logging
+import datetime
+from pathlib import Path
 
-logging.basicConfig(level=logging.INFO, filename='bot.log',
+logs_dir = './bot_log'
+bot_logging_dir = Path(logs_dir)
+bot_logging_dir.mkdir(exist_ok=True)
+
+logging.basicConfig(level=logging.INFO, filename=f'{logs_dir}/start_bot_{datetime.datetime.now().strftime("%d_%m_%y_%H_%M_%S")}.log',
                     format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
 
 def log_database_update(text):
-    '''
-    logger = logging.getLogger()
-    logger.setLevel(logging.INFO)
-    formatter = logging.Formatter('%(asctime)s | %(levelname)s | %(message)s')
-
-    file_handler = logging.FileHandler('logs.log')
-    file_handler.setLevel(logging.DEBUG)
-    file_handler.setFormatter(formatter)
-
-    logger.addHandler(file_handler)
-    '''
     logging.info(text)
 
 def log_user_info(user_id, message):
