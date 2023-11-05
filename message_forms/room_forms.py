898b8820176fba36683fdb58042c819799bdce98
AenclaveGames <55506834+IlyaChichkov@users.noreply.github.com>
2023-11-05 19:41:03 +0300

    feat: Добавил в сообщение главного меню комнаты место в очереди для пользователя

--- message_forms/room_forms.py
+++ message_forms/room_forms.py
@@ -40,6 +40,10 @@ async def get_welcome_message(user_id, room: Room):
     room_name = room.name
     moderator_code = room.moderators_join_code
     join_code = room.users_join_code
+    place_message = 'Вас нет в очереди.'
+    if user_id in room.queue:
+        place_message = f'Вы в очереди на {room.queue.index(user_id) + 1} месте.'
+
     role_to_welcome_text = {
         UserRoles.Admin:  f"📖 Вы находитесь в меню комнаты «<b>{room_name}</b>»\n"
                           f"Код для присоединения:\nМодераторов: <tg-spoiler><code>{moderator_code}</code></tg-spoiler>\n"
@@ -47,7 +51,7 @@ async def get_welcome_message(user_id, room: Room):
         UserRoles.Moderator:  f"📖 Вы находитесь в меню комнаты «<b>{room_name}</b>»\n"
                               f"Код для присоединения:\nМодераторов: <tg-spoiler><code>{moderator_code}</code></tg-spoiler>\n"
                               f"Студентов: <code>{join_code}</code>",
-        UserRoles.User:  f'📖 Вы находитесь в меню комнаты «<b>{room_name}</b>»\n'
+        UserRoles.User:  f'📖 Вы находитесь в меню комнаты «<b>{room_name}</b>»\n{place_message}'
     }
     mesg_text = role_to_welcome_text.get(role, 'None')
     return { 'mesg_text': mesg_text, 'keyboard': kb }
