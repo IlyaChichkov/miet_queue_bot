0fad4dfe81296521d6c7188097b82cf1c7437a14
IlyaChichkov <ilya555chich@yandex.ru>
2023-11-13 13:45:49 +0300

    feat: Добавил отправку сообщения главного меню комнаты при изменении очереди

--- handlers/room_welcome.py
+++ handlers/room_welcome.py
@@ -1,6 +1,13 @@
+import logging
+
 from aiogram import Router, types
+
+from bot_conf.bot import bot
+from events.queue_events import update_queue_event
 from firebase_manager.firebase import db_get_user_room, is_user_name_default, get_user_name
 from message_forms.room_forms import get_welcome_message
+from models.room import Room
+from models.server_rooms import get_room
 from states.room_state import RoomVisiterState
 
 router = Router()
@@ -24,6 +31,21 @@ async def welcome_room(message: types.Message, user_id = None):
         await message.answer(f"Произошла ошибка при присоединении к комнате.\n"
                              f"{room['error']}")
 
+
+async def update_welcome_room(room_id, user_id):
+    room: Room = await get_room(room_id)
+    for room_user in room.users:
+        try:
+            user_name = await get_user_name(user_id)
+            if await is_user_name_default(user_id):
+                await bot.send_message(room_user, f'ℹ️ Ваше имя соответствует стандартному: «<b>{user_name}</b>». Пожалуйста поменяйте его в настройках профиля (Имя Фамилия №ПК)', parse_mode="HTML")
+                mesg = await get_welcome_message(room_user, room['room'])
+                await bot.send_message(room_user, mesg['mesg_text'], parse_mode="HTML", reply_markup=mesg['keyboard'])
+        except Exception as ex:
+            logging.info(f"Tried to update main room screen USER_{room_user} (user) because queue changed, but got error: {ex}")
+
+
+update_queue_event.add_handler(update_welcome_room)
 
 @router.message(RoomVisiterState.ROOM_WELCOME_SCREEN)
 async def welcome_room_state(message: types.Message):
