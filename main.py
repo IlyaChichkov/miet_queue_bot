69327559105f2d0bc783033362a16e6e47c2ea92
IlyaChichkov <ilya555chich@yandex.ru>
2023-12-08 12:48:36 +0300

    ref: Перенес импорт роутеров в отдельный файл

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import logging
 
 from dotenv import load_dotenv
 from bot_conf.bot import dp, bot
-from handlers import admin_commands, own_rooms_handler, welcome, room_actions, assign_screen, profile_settings, room_settings, room_welcome, main_screens, queue_screen, queue_settings
+from handlers.dp_router.dp_routers_import import import_routers
 from models.server_admin import load_cache
 
 ''' DO NOT REMOVE | EVENTS '''
@@ -23,17 +23,7 @@ load_dotenv()
 
 async def main():
     logging.info('------------------ Start bot pooling ------------------')
-    dp.include_routers(admin_commands.router)
-    dp.include_routers(queue_settings.router)
-    dp.include_routers(main_screens.router)
-    dp.include_routers(own_rooms_handler.router)
-    dp.include_routers(welcome.router)
-    dp.include_routers(profile_settings.router)
-    dp.include_routers(room_settings.router)
-    dp.include_routers(room_actions.router)
-    dp.include_routers(room_welcome.router)
-    dp.include_routers(queue_screen.router)
-    dp.include_routers(assign_screen.router)
+    await import_routers()
     await load_cache()
     await dp.start_polling(bot)
 
